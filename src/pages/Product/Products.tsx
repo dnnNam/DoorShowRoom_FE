abf6f2cac481edd4b46d7b7d8b9c2695fd5a66d1
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2025-12-31 14:57:14 +0700

    #17  done add search input and filter by category

--- src/pages/Product/Products.tsx
+++ src/pages/Product/Products.tsx
@@ -1,9 +1,11 @@
 import ProductCard from "@/components/ProductCard";
 import { products } from "@/data/product";
+import { Search } from "lucide-react";
 import { useState } from "react";
 
 const categories = ["Tất cả", "Cửa Gỗ", "Cửa Kính", "Cửa Cuốn", "Cửa Nhôm"];
 export default function Products() {
+  const [searchQuery, setSearchQuery] = useState("");
   const [activeCategory, setActiveCategory] = useState("Tất cả");
   const filteredProducts =
     activeCategory === "Tất cả"
@@ -22,6 +24,34 @@ export default function Products() {
             Chúng tôi tự tin đáp ứng mọi yêu cầu khắt khe nhất của bạn.
           </p>
         </div>
+
+        {/* Search Bar */}
+
+        <section className="sticky top-20 z-40 bg-white/90 backdrop-blur-sm py-3 border-b mb-4 border-gray-100 shadow-sm">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="max-w-2xl mx-auto mb-6">
+              <div className="relative mt-8">
+                <Search className="absolute left-4 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-400 " />
+                <input
+                  type="text"
+                  value={searchQuery}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                  placeholder="Tìm kiếm sản phẩm theo tên, loại cửa..."
+                  className="w-full pl-12 pr-4 py-3 text-base border-2 border-gray-200 rounded-lg"
+                />
+                {searchQuery && (
+                  <button
+                    onClick={() => setSearchQuery("")}
+                    className="absolute right-4 top-1/2 -translate-y-1/2"
+                  >
+                    ✕
+                  </button>
+                )}
+              </div>
+            </div>
+          </div>
+        </section>
+
         {/* Filters */}
         <div className="flex flex-wrap justify-center gap-3 mb-12">
           {categories.map((cat) => (
