74ec5a19116071bc17c629bd2e4aed6dda2ecd29
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2026-01-30 12:52:42 +0700

    #58 done config api

--- src/apis/products.apis.ts
+++ src/apis/products.apis.ts
@@ -1,11 +1,17 @@
-import type { Product, SuccessResponse } from "~/types/api/product.type";
+import type {
+  Product,
+  ProductListReponse,
+  SuccessResponse,
+} from "~/types/api/product.type";
 import type { UseProductsParams } from "~/types/domain/product.type";
 
 import http from "~/utils/http";
 
 const productApis = {
   getAllProducts(query: UseProductsParams) {
-    return http.get<SuccessResponse<Product[]>>("/products", { params: query });
+    return http.get<SuccessResponse<ProductListReponse>>("/products", {
+      params: query,
+    });
   },
 
   getProductById(productId: number) {
